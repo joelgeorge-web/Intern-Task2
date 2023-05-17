e09a440a85cd3a1c8a2bc732497c3ffa04259066
joelgerge-web <joelgeorge98765@gmail.com>
2023-05-17 12:39:59 +0530

    new card reading

--- ocr.py
+++ ocr.py
@@ -11,7 +11,7 @@ mydb = mysql.connector.connect(
 api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDExNDk3ODIzODk3ODczMDU0NzM3MiIsImVtYWlsIjoiMjBjdDA5N0BtZ2l0cy5hYy5pbiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODQzMDA0NjIzNzZ9.e9xetvl0i9tFVT1JOpRimSMptco5AdLKtKnOYW4Y-N4'
 queue_id = '95f84c64-3d4a-4537-b6be-559da7a7240b'
 
-file_location = "id1.jpg"
+file_location = "id2.jpg"
 
 results = Client(api_key).extract_document(queue_id, file_location)
 
