0c4e8013a9b868f97d11e5030da0f1fdfd3ecfee
joelgerge-web <joelgeorge98765@gmail.com>
2023-05-17 11:07:08 +0530

    working

--- ocr.py
+++ ocr.py
@@ -1,12 +1,12 @@
-# Make sure to first install the SDK using 'pip install butler-sdk'
+
 from butler import Client
 
-# Specify variables for use in script below
+
 api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDExNDk3ODIzODk3ODczMDU0NzM3MiIsImVtYWlsIjoiMjBjdDA5N0BtZ2l0cy5hYy5pbiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODQzMDA0NjIzNzZ9.e9xetvl0i9tFVT1JOpRimSMptco5AdLKtKnOYW4Y-N4'
 queue_id = '95f84c64-3d4a-4537-b6be-559da7a7240b'
 
-# Specify the path to the file you would like to process
-file_location = '/path/to/file'
+
+file_location = "WIN_20230517_11_02_17_Pro.jpg"
 
 response = Client(api_key).extract_document(queue_id, file_location)
 print(response)
