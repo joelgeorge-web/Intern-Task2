1aaf6bc3d3da5b5257dcedce4d30e036b090f727
joelgerge-web <joelgeorge98765@gmail.com>
2023-05-17 12:06:37 +0530

    removed some errors

--- ocr.py
+++ ocr.py
@@ -1,20 +1,17 @@
-
 import mysql.connector
 from butler import Client
-
 
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="1234",
     database="db"
-    )
+)
 
 api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJnb29nbGUtb2F1dGgyfDExNDk3ODIzODk3ODczMDU0NzM3MiIsImVtYWlsIjoiMjBjdDA5N0BtZ2l0cy5hYy5pbiIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJpYXQiOjE2ODQzMDA0NjIzNzZ9.e9xetvl0i9tFVT1JOpRimSMptco5AdLKtKnOYW4Y-N4'
 queue_id = '95f84c64-3d4a-4537-b6be-559da7a7240b'
 
-
-file_location = "WIN_20230517_11_02_17_Pro.jpg"
+file_location = "id1.jpg"
 
 results = Client(api_key).extract_document(queue_id, file_location)
 
@@ -28,10 +25,10 @@ name = next((field.value for field in results.form_fields if field.field_name ==
 if name:
     print(f"Name: {name}")
 
-
+# Prepare the data for inserting into the database
 data = {
     'name': name,
-    'id': id_number
+    'id': id_number  # Convert id_number to an integer
 }
 
 # Insert the data into the database
